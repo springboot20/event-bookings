981f03b0b9b0a19eb2b006834cfd4271503ebbbd
Yunus Abbas Opeyemi <abbasopeyemi148@gmail.com>
2024-11-24 14:23:44 +0100

    file base system routing logic modified and updated

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,11 +1,11 @@
-import BookingLayout from '../layout/BookingLayout';
-import { PublicRoute } from '../components/PublicRoute';
-import AuthLayout from '../layout/AuthLayout';
-import Login from '../pages/login';
-import Register from '../pages/register';
-import { PrivateRoute } from '../components/PrivateRoute';
-import { AcceptedPersmissonRoles } from '../util';
-import { createBrowserRouter } from 'react-router-dom';
+import BookingLayout from "../layout/BookingLayout";
+import { PublicRoute } from "../components/PublicRoute";
+import AuthLayout from "../layout/AuthLayout";
+import Login from "../pages/login";
+import Register from "../pages/register";
+import { PrivateRoute } from "../components/PrivateRoute";
+import { AcceptedPersmissonRoles } from "../util";
+import { createBrowserRouter, Navigate } from "react-router-dom";
 
 type PagesTypes = Record<
   string,
@@ -25,81 +25,91 @@ type RouteType = {
   ErrorBoundary: React.ComponentType;
 };
 
+const authroutes = () => ({
+  element: (
+    <PublicRoute>
+      <AuthLayout />
+    </PublicRoute>
+  ),
+  children: [
+    {
+      path: "login",
+      element: <Login />,
+    },
+    {
+      path: "register",
+      element: <Register />,
+    },
+    {
+      path: "logout",
+      element: <Navigate to={"/login"} replace state={{ path: window.location.pathname }} />,
+    },
+  ],
+});
+
+const bookingsroutes = (routes: RouteType[]) => {
+  return {
+    element: <BookingLayout />,
+    children: routes.map(({ Element, ErrorBoundary, ...rest }) => ({
+      ...rest,
+      element: getRouteElement(rest.path, Element),
+      ...(ErrorBoundary && { errorElement: <ErrorBoundary /> }),
+    })),
+  };
+};
+const getRouteElement = (path: string, Element: React.ComponentType): JSX.Element => {
+  const isProtectedFile = path.split("/").includes("create") || path.split("/").includes("edit");
+  if (isProtectedFile) {
+    return (
+      <PrivateRoute roles={[AcceptedPersmissonRoles.ADMIN]}>
+        <Element />
+      </PrivateRoute>
+    );
+  }
+
+  if (path === "/") {
+    return <Element />;
+  }
+
+  return (
+    <PrivateRoute roles={[AcceptedPersmissonRoles.USER, AcceptedPersmissonRoles.ADMIN]}>
+      <Element />
+    </PrivateRoute>
+  );
+};
+
+const transformPath = (filename: string): string => {
+  if (filename === "index") return "/";
+
+  return filename.includes("$")
+    ? filename.replace("$", ":").toLowerCase()
+    : filename.replace(/\/index/, "").toLowerCase();
+};
+
 export const router = () => {
-  const pages: PagesTypes = import.meta.glob('../pages/**/*.tsx', { eager: true });
+  const pages: PagesTypes = import.meta.glob("../pages/**/*.tsx", { eager: true });
 
   const routes: RouteType[] = [];
-  let filename: string | undefined;
-  for (const path of Object.keys(pages)) {
-    filename = path.match(/\.\/pages\/(.*)\.tsx$/)?.[1];
 
-    if (!filename) {
-      continue;
-    }
+  Object.keys(pages).reduce<RouteType[]>((acc, path) => {
+    let filename = path.match(/\.\/pages\/(.*)\.tsx$/)?.[1];
 
-    const pathname = filename.includes('$')
-      ? filename.replace('$', ':')
-      : filename.replace(/\/index/, '');
+    if (!filename) return acc;
+
+    const pathname = transformPath(filename);
 
     routes.push({
-      path: filename === 'index' ? '/' : `/${pathname.toLowerCase()}`,
+      path: pathname,
       Element: pages[path]?.default,
       loader: pages[path]?.loader,
       action: pages[path]?.action,
       ErrorBoundary: pages[path]?.ErrorBoundary,
     });
-  }
 
-  const router = createBrowserRouter(
-    routes.map(({ Element, ErrorBoundary, ...rest }) => {
-      const { path } = rest;
-      return {
-        children: [
-          {
-            element: <BookingLayout />,
-            children: [
-              {
-                ...rest,
-                element:
-                  path.split('/').includes('create') || path.split('/').includes('edit') ? (
-                    <PrivateRoute roles={[AcceptedPersmissonRoles.ADMIN]}>
-                      <Element />
-                    </PrivateRoute>
-                  ) : path === '/' ? (
-                    <PublicRoute>
-                      <Element />
-                    </PublicRoute>
-                  ) : (
-                    <PrivateRoute
-                      roles={[AcceptedPersmissonRoles.USER, AcceptedPersmissonRoles.ADMIN]}>
-                      <Element />
-                    </PrivateRoute>
-                  ),
-                ...(ErrorBoundary && { errorElement: <ErrorBoundary /> }),
-              },
-            ],
-          },
-          {
-            element: (
-              <PublicRoute>
-                <AuthLayout />
-              </PublicRoute>
-            ),
-            children: [
-              {
-                path: 'login',
-                element: <Login />,
-              },
-              {
-                path: 'register',
-                element: <Register />,
-              },
-            ],
-          },
-        ],
-      };
-    })
-  );
+    return acc;
+  }, []);
+
+  const router = createBrowserRouter([authroutes(), bookingsroutes(routes)]);
 
   return router;
 };
